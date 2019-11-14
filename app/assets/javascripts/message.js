40a5d76672e764734ac5955b4e01d82ae0e5bc10
tikayk <tikayk@gmail.com>
2019-11-14 18:27:40 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,21 @@
 $(function(){
   function buildHTML(message){
     let image = message.image? `<img class="input-box__image" src=${message.image}>` : "";
-    let html = `<div class = 'message'>
-    <div class="message__upper-info">
-                  <p class="message__upper-info__talker">
-                    ${message.name}
-                  </p>
-                  <p class="message__upper-info__date">
-                    ${message.date}
-                  </p>
-                </div>
-                <div class="lower-message">
-                  <p class="message__text">
-                    ${message.content}
-                  </p>
-                  ${image}
-                </div>
+    let html = `<div class = 'message' data-message-id='${message.id}'>
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                      ${message.name}
+                    </p>
+                    <p class="message__upper-info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <div class="lower-message">
+                    <p class="message__text">
+                      ${message.content}
+                    </p>
+                    ${image}
+                  </div>
                 </div>`
     return html;
   }
@@ -31,7 +31,6 @@ $(function(){
       processData: false,
       contentType: false
     })
-
     .done(function(data){
       let html = buildHTML(data);
       $('.messages').append(html);
@@ -43,4 +42,30 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
+
+
+  let reloadMessages = function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      let last_message_id = $('.message:last').data('message-id');
+      $.ajax({
+        url: 'api/messages',
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages){
+        let insertHTML = '';
+        messages.forEach(function(message){
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        });
+      })
+      .fail(function(){
+        console.log('error');
+      })
+    };
+  };
+  setInterval(reloadMessages,3000);
 });
